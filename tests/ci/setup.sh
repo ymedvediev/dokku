05b3bea8c9214e2feaf38ae5225b3364d813846c
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2021-01-11 02:04:39 -0500

    fix: add debconf-utils for debconf-get-selections

--- tests/ci/setup.sh
+++ tests/ci/setup.sh
@@ -29,7 +29,7 @@ install_dependencies() {
 
   sudo add-apt-repository -y ppa:nginx/stable
   sudo apt-get update -qq
-  sudo apt-get -qq -y --no-install-recommends install cgroupfs-mount dos2unix jq nginx
+  sudo apt-get -qq -y --no-install-recommends install cgroupfs-mount dos2unix jq nginx debconf-utils
   sudo cp "${ROOT_DIR}/tests/dhparam.pem" /etc/nginx/dhparam.pem
 
   sudo dpkg -i "${ROOT_DIR}/build/$HEROKUISH_PACKAGE_NAME" \
